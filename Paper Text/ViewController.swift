ebbc15e3232456f6b99d25713671e147119f4d7d
Roy Bailey <baileyii.roy@gmail.com>
2019-11-30 21:13:13 -0800

    Initial Commit

---
+++ Paper Text/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Paper Text
+//
+//  Created by Roy Bailey II on 11/30/19.
+//  Copyright © 2019 Roy Bailey II. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
